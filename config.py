ac3abcd06c6b7fc360a513753bf9b84e6968671e
shashankv777 <shashankv9353@gmail.com>
2026-07-29 11:16:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,41 @@
+# Dummy / fake secrets for CNS secret-scanning demo. Do NOT use in production.
+
+################################################################################
+# Cloud Service Providers
+################################################################################
+
+## AWS
+AWS_ACCESS_KEY_ID="AKIAX24QKKOLIBKF7D5X"
+AWS_SECRET_ACCESS_KEY="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"
+
+## Azure
+AZURE_CLIENT_ID="08a6587c-5a5a-43ca-8c3b-f13f12bb1c26"
+AZURE_TENANT_ID="2ef92694-2c89-448d-ab1f-8db7a336d6c7"
+AZURE_CLIENT_SECRET="u8o8Q~R6vVJ-fjnHSlkz3wFYXQDrSeRWvxw6sa6q"
+
+## GCP
+GOOGLE_API_KEY='AIzaSyBUPHAjZl3n8Eza66ka6B78iVyPteC5MgM'
+
+################################################################################
+# AI
+################################################################################
+
+## Open AI
+OPENAI_API_KEY='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'
+
+################################################################################
+# Development
+################################################################################
+
+## GitHub
+
+## Google Captcha
+GOOGLE_CAPTCHA_KEY='6Lrjv_b_jgnybWRwKSn2P6lop58PGZ_NfewZWnRT'
+
+
+################################################################################
+# Payment Services
+################################################################################
+
+## Stripe
+STRIPE_SECRET_KEY='sk_test_51L53TeSBwzby5YcBooVe7xqrw9DrV7SWyW5WKKYGSFO4fNSoDyd167DZrmIW6lZE6pHUDrYe9zfqGnCHTQUKhnPc00eL5w5pxY'
