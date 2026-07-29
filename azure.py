c613d7edb004026c792755cdf63133d4dfc4b019
shashankv777 <shashankv9353@gmail.com>
2026-07-29 11:10:46 +0530

    Create azure.py

---
+++ azure.py
@@ -0,0 +1,5 @@
+Azure Application Id = 08a6587c-5a5a-43ca-8c3b-f13f12bb1c26
+
+Azure Tenant Id = 2ef92694-2c89-448d-ab1f-8db7a336d6c7
+
+Azure Client Secret = u8o8Q~R6vVJ-fjnHSlkz3wFYXQDrSeRWvxw6sa6q
